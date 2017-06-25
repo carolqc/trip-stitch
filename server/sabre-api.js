574b610c0c0c7216f059b1808490480022a7a4df
ro6 <ro6@users.noreply.github.com>
2017-06-25 13:24:47 -0700

    need to make sure to pick an origin/destination pair they have data for in the test environment

--- server/sabre-api.js
+++ server/sabre-api.js
@@ -1,23 +1,30 @@
 import sabre_flight_api from 'sabre-dev-studio/lib/sabre-dev-studio-flight'
 
 const flight_api = new sabre_flight_api({
-   client_id: 'V1:zyaudm2ybwf9rfzs:DEVCENTER:EXT',
-   client_secret: 'C0uhAcC6',
-   uri: 'https://api.test.sabre.com',
+    client_id: 'V1:zyaudm2ybwf9rfzs:DEVCENTER:EXT',
+    client_secret: 'C0uhAcC6',
+    uri: 'https://api.test.sabre.com',
 })
 
 console.log('run')
 
-const logger_callback = function(error, data) {
-   if (error) {
-      // Your error handling here
-      console.log(error)
-   }
-   else {
-      // Your success handling here
-      console.log(JSON.stringify(JSON.parse(data)))
-   }
+const logger_callback = function (error, data) {
+    if (error) {
+        // Your error handling here
+        console.log(error)
+    }
+    else {
+        // Your success handling here
+        console.log(JSON.stringify(JSON.parse(data)))
+    }
 }
 
 // flight_api.travel_theme_lookup(logger_callback)
-// flight_api.airports_at_cities_lookup({ city: 'NYC' }, logger_callback)
+// flight_api.airports_at_cities_lookup({city: 'NYC'}, logger_callback)
+
+flight_api.instaflights_search({
+    origin: 'DFW',
+    destination: 'LAS',
+    departuredate: '2017-06-28',
+    returndate: '2017-07-01'
+}, logger_callback)
