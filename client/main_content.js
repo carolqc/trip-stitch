b8aa582fb783b7d5f85d5c0a904b75efcfeb5fc9
ro6 <ro6@users.noreply.github.com>
2017-06-25 09:27:56 -0700

    switched main_content.js to use template2

--- client/main_content.js
+++ client/main_content.js
@@ -1,35 +1,30 @@
 import {Trips} from '../collections'
 
-// https://stackoverflow.com/questions/28885479/meteor-js-add-a-date-picker-to-a-page
-// $('.datetimepicker').datetimepicker();
+Template2('main_content', {
+    onRendered() {
+        $("#datepicker_start").datetimepicker()
+        $("#datepicker_end").datetimepicker()
+    },
+    events: {
+        'click #finishButton': function (event) {
+            // console.log("event: ",event)
+            event.preventDefault()
+            // console.log(event.type)
+            // console.log("Form submitted")
 
-// Meteor.startup(function() {
-Template.main_content.onRendered(function(){
-    $("#datepicker_start").datetimepicker(),
-    $("#datepicker_end").datetimepicker()
-})
+            var TripType = $('input[name=trip]:checked', '#firstForm').val();
+            // console.log("TripType:  ", TripType);  Value
 
-Template.main_content.events( {
-
-        'click #finishButton': function(event){
-        // console.log("event: ",event)
-        event.preventDefault()
-        // console.log(event.type)
-        // console.log("Form submitted")
-
-        var TripType = $('input[name=trip]:checked', '#firstForm').val();
-        // console.log("TripType:  ", TripType);  Value
-
-        var destination = $("#destination").val();
-        // console.log("destination: ", destination);
-        // var destination = event.target.destination.value;
-        var datepicker_start = $("#datepicker_start").val();
-        var datepicker_end = $("#datepicker_end").val();
-        var organizer_name = $('#organizer_name').val();
-        var organizer_email = $('#organizer_email').val();
-        var trip_title = $('#tripTitle').val();
-        console.log("TripType:  ", TripType, "destination: ", destination, "Start Date: ", datepicker_start );
-        console.log( "End Date: ", datepicker_end, "Name: ", organizer_name, "Email: ", organizer_email, "Trip Title: ", trip_title);
+            var destination = $("#destination").val();
+            // console.log("destination: ", destination);
+            // var destination = event.target.destination.value;
+            var datepicker_start = $("#datepicker_start").val();
+            var datepicker_end = $("#datepicker_end").val();
+            var organizer_name = $('#organizer_name').val();
+            var organizer_email = $('#organizer_email').val();
+            var trip_title = $('#tripTitle').val();
+            console.log("TripType:  ", TripType, "destination: ", destination, "Start Date: ", datepicker_start);
+            console.log("End Date: ", datepicker_end, "Name: ", organizer_name, "Email: ", organizer_email, "Trip Title: ", trip_title);
 
 // how do I send data to server, via function call arguments?
 
@@ -45,25 +40,27 @@ Template.main_content.events( {
             })
 
             FlowRouter.go(`/trips/${trip_id}`)
-    },
+        },
 
-    // https://stackoverflow.com/questions/24869542/calling-javascript-function-on-button-click-meteor
-    'click .AddTravelerButton': function(event){
-      event.preventDefault();
-      console.log("You pressed the add traveler button");
+        // https://stackoverflow.com/questions/24869542/calling-javascript-function-on-button-click-meteor
+        'click .AddTravelerButton': function (event) {
+            event.preventDefault();
+            console.log("You pressed the add traveler button");
 
-      var travelerName = $("#travelerName").val();
-      var travelerAirPort = $("#travelerAirPort").val();
-      var travelerEmail = $("#travelerEmail").val();
-      var travelerPhone = $("#travelerPhone").val();
+            var travelerName = $("#travelerName").val();
+            var travelerAirPort = $("#travelerAirPort").val();
+            var travelerEmail = $("#travelerEmail").val();
+            var travelerPhone = $("#travelerPhone").val();
 
-    //   Clear out the entries
-    $("#travelerName").val("")
-    $("#travelerAirPort").val("")
-    $("#travelerEmail").val("")
-    $("#travelerPhone").val("")
 
-      // need to send values to server, then paste the data below, and then clear out the cells.
+            //   Clear out the entries
+            $("#travelerName").val("")
+            $("#travelerAirPort").val("")
+            $("#travelerEmail").val("")
+            $("#travelerPhone").val("")
 
-  }
+            // need to send values to server, then paste the data below, and then clear out the cells.
+
+        }
+    }
 })
