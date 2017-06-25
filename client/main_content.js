f41ada4a897448e12bae6dfc312341d421646f6f
ro6 <ro6@users.noreply.github.com>
2017-06-25 11:23:25 -0700

    initial attempt at displaying added travelers

--- client/main_content.js
+++ client/main_content.js
@@ -1,49 +1,70 @@
 import {Trips} from '../collections'
 
-Template2('main_content', {
-    onRendered() {
-        $("#datepicker_start").datetimepicker()
-        $("#datepicker_end").datetimepicker()
+Template.main_content.onCreated(function () {
+    this.current_trip_id = new ReactiveVar(null)
+    this.travelers = new ReactiveVar([])
+})
+
+Template.main_content.onRendered(function () {
+    $("#datepicker_start").datetimepicker()
+    $("#datepicker_end").datetimepicker()
+})
+
+Template.main_content.helpers({
+    trip_data() {
+        return Trips.findOne(Template.instance().current_trip_id.get())
     },
-    events: {
-        'click #finishButton': function (event) {
+    travelers() {
+        return Template.instance().travelers.get()
+    }
+})
+
+Template.main_content.events({
+        'click #finishButton': function (event, template) {
             // console.log("event: ",event)
             event.preventDefault()
             // console.log(event.type)
             // console.log("Form submitted")
 
-            var TripType = $('input[name=trip]:checked', '#firstForm').val();
-            // console.log("TripType:  ", TripType);  Value
+            var trip_type = $('input[name=trip]:checked', '#firstForm')
+            // console.log("trip_type:  ", trip_type);  Value
 
-            var destination = $("#destination").val();
+            var destination = $("#destination")
             // console.log("destination: ", destination);
             // var destination = event.target.destination.value;
-            var datepicker_start = $("#datepicker_start").val();
-            var datepicker_end = $("#datepicker_end").val();
-            var organizer_name = $('#organizer_name').val();
-            var organizer_email = $('#organizer_email').val();
-            var trip_title = $('#tripTitle').val();
-            console.log("TripType:  ", TripType, "destination: ", destination, "Start Date: ", datepicker_start);
+            var datepicker_start = $("#datepicker_start")
+            var datepicker_end = $("#datepicker_end")
+            var organizer_name = $('#organizer_name')
+            var organizer_email = $('#organizer_email')
+            var trip_title = $('#tripTitle')
+            console.log("trip_type:  ", trip_type, "destination: ", destination, "Start Date: ", datepicker_start);
             console.log("End Date: ", datepicker_end, "Name: ", organizer_name, "Email: ", organizer_email, "Trip Title: ", trip_title);
 
-// how do I send data to server, via function call arguments?
-
             const trip_id = Trips.insert({
-                trip_type: TripType,
-                destination: destination,
-                start_date: datepicker_start,
-                end_date: datepicker_end,
-                organizer_name: organizer_name,
-                organizer_email: organizer_email,
-                trip_title: trip_title,
+                trip_type: trip_type.val(),
+                destination: destination.val(),
+                start_date: datepicker_start.val(),
+                end_date: datepicker_end.val(),
+                organizer_name: organizer_name.val(),
+                organizer_email: organizer_email.val(),
+                trip_title: trip_title.val(),
                 travelers: []
             })
 
-            FlowRouter.go(`/trips/${trip_id}`)
-        },
+            template.current_trip_id.set(trip_id)
+
+            trip_type.val('')
+            destination.val('')
+            datepicker_start.val('')
+            datepicker_end.val('')
+            organizer_name.val('')
+            organizer_email.val('')
+        }
+
+        ,
 
         // https://stackoverflow.com/questions/24869542/calling-javascript-function-on-button-click-meteor
-        'click .AddTravelerButton': function (event) {
+        'click .AddTravelerButton': function (event, template) {
             event.preventDefault();
             console.log("You pressed the add traveler button");
 
@@ -52,15 +73,19 @@ Template2('main_content', {
             var travelerEmail = $("#travelerEmail").val();
             var travelerPhone = $("#travelerPhone").val();
 
+            template.travelers.set(template.travelers.get().concat([{
+                name: travelerName,
+                origin_airport_code: travelerAirPort,
+                email: travelerEmail,
+                phone: travelerPhone
+            }]))
 
-            //   Clear out the entries
+            console.log('travelers', template.travelers.get())
+
             $("#travelerName").val("")
             $("#travelerAirPort").val("")
             $("#travelerEmail").val("")
             $("#travelerPhone").val("")
-
-            // need to send values to server, then paste the data below, and then clear out the cells.
-
         }
     }
-})
+)
