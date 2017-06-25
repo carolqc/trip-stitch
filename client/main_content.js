27ab830548399bfd211ab05b4e602dde001c49c2
ro6 <ro6@users.noreply.github.com>
2017-06-25 06:31:47 -0700

    autopublish and insecure so we can just use the server db like it's local to the client

--- client/main_content.js
+++ client/main_content.js
@@ -1,15 +1,17 @@
+import {Trips} from '../collections'
 import {Template} from 'meteor/templating'
 
 Template.main_content.events({
     'submit form[name=create-trip]': function (event) {
         event.preventDefault()
 
-        Meteor.call('create-a-trip', {
+        Trips.insert({
             destination_airport_code: event.target.destination.value,
             start_date: event.target.datepicker_start.value,
             end_date: event.target.datepicker_end.value,
             organizer_name: event.target.organizer_name.value,
-            organizer_email: event.target.organizer_email.value
-        });
+            organizer_email: event.target.organizer_email.value,
+            participants: []
+        })
     }
 })
--- server/methods.js
+++ server/methods.js
@@ -1,7 +1,2 @@
 import {Trips} from '../collections'
 
-Meteor.methods({
-    'create-a-trip'(data) {
-        Trips.insert(data)
-    }
-});
